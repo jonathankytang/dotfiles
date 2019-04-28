06435c7c1dd1becc146bb76974a17c92ef1b67e7
Jonathan Tang <jonathankytang@gmail.com>
2019-04-28 01:42:35 +0100

    adding git command aliases

---
+++ .git.sh
@@ -0,0 +1,10 @@
+# General git helpers
+
+alias ga="git add ."
+alias gb="git branch"
+alias gc="git commit"
+alias gd="git diff"
+alias gl="git log --decorate --color --oneline"
+alias gp="git push -u"
+
+alias gs="git status"
